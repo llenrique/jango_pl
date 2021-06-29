cc0c106989859fc1417c112315aff1007725b845
EL <llenriquelopez@gmail.com>
2021-06-29 16:38:35 -0500

    Removes secret key

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)bq5ecm=-7pv*7=tnq%)zl+taoz2wnhcp2=hy5nx*!qogna$(#'
+SECRET_KEY = 'django_secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
